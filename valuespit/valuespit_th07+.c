a9d557ea76a383fae54dc3729aaea455c258777a
nmlgc <nmlgc@nmlgc.net>
2011-08-16 19:44:46 +0200

    valuespit: Directly output in the actual .bgm key format for TH07+

--- valuespit/valuespit_th07+.c
+++ valuespit/valuespit_th07+.c
@@ -11,12 +11,15 @@ void strfmt(char* str);
 int main(int argc, char** argv)
 {
 	FILE *In, *Out;
-	short Upper;
-	char Name[16], Fmt[64];
-	unsigned long Start, Loop, End;
-	int Len, c;
+	char Name[16], Fmt[256];
+	unsigned long Start, Loop, End, Freq;
+	int c = 1;
 
-	if(argc < 2)	In = fopen("thbgm.fmt", "rb");
+	if(argc < 2)
+	{
+		In = fopen("thbgm.fmt", "rb");
+		if(!In)	In = fopen("thbgm_tr.fmt", "rb");
+	}
 	else		In = fopen(argv[1], "rb");
 	if(!In)
 	{
@@ -35,18 +38,24 @@ int main(int argc, char** argv)
 	while(!feof(In))
 	{
 		fread(Name, 16, 1, In);
-		if(Name[0] == '\0')	return;
+		if(Name[0] == '\0')	break;
 
 		fread(&Start, 4, 1, In);
 		fseek(In, 4, SEEK_CUR);
 		fread(&Loop, 4, 1, In);
 		fread(&End, 4, 1, In);
-		fread(Fmt, 20, 1, In);
+		fseek(In, 4, SEEK_CUR);
+		fread(&Freq, 4, 1, In);
+		fread(Fmt, 12, 1, In);
 
-		sprintf(Fmt, "%s:ß0x%8x,ß0x%8x,ß0x%8x\n", Name, Start, Loop, End);
-		strfmt(Fmt);
+		sprintf(Fmt, "[%02d]\n"
+				"name_jp = \"\"\n"
+				"filename = \"%s\"\n"
+				"position = \"%#08x, %#08x, %#08x\"\n"
+				"frequency = %d\n\n", c, Name, Start, Loop, End, Freq);
 
 		fputs(Fmt, Out);
+		c++;
 	}
 	fclose(In);
 	fclose(Out);
